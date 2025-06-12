b0a3cb83192d5f1a8876d4d5495cec1f34252319
Dorian <dorian12995@gmail.com>
2025-06-12 16:32:43 +0300

    add root controller

---
+++ AdWriter-AI/Controllers/RootController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdWriter_AI.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("AdWriter AI API is running.");
+        }
+    }
+}
